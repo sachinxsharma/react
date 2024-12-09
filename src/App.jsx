993bf48dab1a6c7762c68308aa86fc626db347ff
sachinxsharma <sachinsharma72003@gmail.com>
2024-12-09 17:13:40 +0530

    wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,25 @@
-import React , {useState} from 'react';
+// import React , {useState} from 'react';
 import './App.css'
 
-let counter = 4;
 function App() {
-  const [todos , setTodos ] = useState([{
-    id: 1,
-    title: "go to gym",
-    description: "go to gym today"
-  },{
-      id: 2,
-      title: "go to school",
-      description: "go to school today"
-    }, {
-      id: 3,
-      title: "go to market",
-      description: "go to market today"
-    }
-  ])
-
-  function addTodo(){
-    // spread operator and add the new todo 
-    setTodos([...todos,{
-      id:counter++,
-      title: Math.random(),
-      description: Math.random()
-    }])
-    console.log(setTodos)
-
-  }
-  return (
-    <div>
-       <button onClick={addTodo}> add a new todo</button>
-       {todos.map(todo => <Todo title = {todo.title} key={todo.id} description = {todo.description} />)}
-    </div>
-  )
-}
-function Todo({title, description}){
   return <div>
-    <h1>
-      {title}
-    </h1>
-    <h5>
-      {description}
-    </h5>
+  <cardWrapper innerComponent={<TextComponent/>} />
+  <cardWrapper innerComponent={<TextComponent2/>} />
   </div>
 }
-
+function cardWrapper({innerComponent}){
+  return <div style = {{border: "2px solid black", padding: 20}}>
+    {innerComponent}
+  </div>
+}
+function TextComponent(){
+  return <div>
+    hi there 
+  </div>
+}
+function TextComponent2(){
+  return <div>
+    hi there , my name is sachin
+  </div>
+}
 export default App
