8251b156711917b239f28174c1be42acd58f4fad
sachinxsharma <sachinsharma72003@gmail.com>
2024-12-09 17:19:38 +0530

    real wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 // import React , {useState} from 'react';
 import './App.css'
 
+
+// real wrapper component , 
+// what do i mean by when i say wrapper - writing a single card struture {where all my styles div, etc etc }
+//  in the main component i can call the wrapper and use it 
 function App() {
-  return <div>
-  <cardWrapper innerComponent={<TextComponent/>} />
-  <cardWrapper innerComponent={<TextComponent2/>} />
-  </div>
-}
-function cardWrapper({innerComponent}){
-  return <div style = {{border: "2px solid black", padding: 20}}>
-    {innerComponent}
-  </div>
-}
-function TextComponent(){
-  return <div>
+  return <cardWrapper>
     hi there 
+  </cardWrapper>
+}
+function cardWrapper({children}){
+  return <div style = {{border: "2px solid black", padding: 20}}>
+    {children}
   </div>
 }
-function TextComponent2(){
-  return <div>
-    hi there , my name is sachin
-  </div>
-}
+
+
 export default App
